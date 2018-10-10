3c1b7bb850b8fa7dbae6e39e1d42a6ef8d5c6494
stephanie.mcginty <stephanie.mcginty@bellevuecollege.edu>
2018-10-10 12:24:17 -0700

    list button

---
+++ ListButton/index.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    const userInput = document.getElementById('list-data');    
+    const setTextButton = document.getElementById('set-text');
+
+    setTextButton.onclick = () => {
+        userInput.value = 'The first item for my list';
+     }
+}
