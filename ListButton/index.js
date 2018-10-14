37356cb97fd9d7bee517678988b19f33d2f7bb59
Stephanie McGinty <stephaniemcginty@Stephanies-MacBook-Pro.local>
2018-10-14 14:08:39 -0700

    added to listbutton assignment

--- ListButton/index.js
+++ ListButton/index.js
@@ -1,8 +1,17 @@
 window.onload = () => {
+    // Part I here
     const userInput = document.getElementById('list-data');    
     const setTextButton = document.getElementById('set-text');
+    const listButton = document.getElementById('add-to-list');
 
+    // Part II here
     setTextButton.onclick = () => {
         userInput.value = 'The first item for my list';
      }
+
+    // Part III here
+     listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, userInput.value);
+    }
 }
