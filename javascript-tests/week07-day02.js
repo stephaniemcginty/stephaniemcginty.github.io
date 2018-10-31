33f8a613cbf622bb6a5e68bb7c97799529c3d34a
stephanie.mcginty <stephanie.mcginty@bellevuecollege.edu>
2018-10-31 12:22:36 -0700

    for loops assignment

--- javascript-tests/week07-day02.js
+++ javascript-tests/week07-day02.js
@@ -1,3 +1,31 @@
-const result = 1 + 2 * 3;
+var showMessage = (message) => {
+    console.log("======================");
+    console.log("== " + message);
+    console.log("======================");
+}
 
+showMessage('Operator Precedance');
+
+let result = 1 + (2 * 3);
 console.log(result);
+
+result = (1 + 2) * 3;
+console.log(result);
+
+showMessage('For Loops');
+
+for (let i = 10; i <= 25; i = i + 2) {
+    console.log(i);
+}
+
+showMessage('While Loops');
+
+let looper = 10;
+
+while (looper < 15) {
+    console.log(looper++);
+}
+
+do {
+    console.log(looper++);
+} while (looper < 20);
