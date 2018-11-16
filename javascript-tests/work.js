b35af1849c01af729229349df633f392530a4e32
Stephanie McGinty <stephanie.mcginty@bellevuecollege.edu>
2018-11-15 20:45:11 -0800

    completed javascript objects assignment

--- javascript-tests/work.js
+++ javascript-tests/work.js
@@ -1,5 +1,5 @@
 var person = {
-    firstName: "Stephanie ",
+    firstName: "Stephanie",
     lastName: "McGinty",
     fullName: function () { 'use strict';
         return this.firstName + ' ' + this.lastName;
@@ -7,6 +7,44 @@ var person = {
     }
 };
 
+var calculator = {
+    operand01: -1,
+    operand02: -1,
+    add: function () {'use strict';
+        return this.operand01 + this.operand02;
+    },
+    subtract: function () {'use strict';
+        return this.operand01 - this.operand02;
+
+    }
+};
+
+calculator.multiply = function () {'use strict';
+    return calculator.operand01 * calculator.operand02;
+
+}
+
+
+
+function divider(title) {
+    console.log("====================================");
+    console.log(title);
+    console.log("====================================");
+}
+
+divider('Person');
+
 console.log(person.firstName);
 console.log(person.lastName);
 console.log(person.fullName());
+
+divider('Calculator');
+
+calculator.operand01 = person.firstName.length;
+calculator.operand02 = person.lastName.length;
+console.log('operand01:', calculator.operand01);
+console.log('operand02:', calculator.operand02);
+
+console.log('add:', calculator.add());
+console.log('subtract:', calculator.subtract());
+console.log('multiply:', calculator.multiply());
