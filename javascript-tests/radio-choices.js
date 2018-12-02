e71be55a306c542e99b0b66790524528aab245cb
Stephanie McGinty <stephanie.mcginty@bellevuecollege.edu>
2018-12-02 12:25:42 -0800

    radio choices assignment

--- javascript-tests/radio-choices.js
+++ javascript-tests/radio-choices.js
@@ -4,12 +4,35 @@ window.onload = () => {
     myForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const value = document.querySelector('input[name="sorter"]:checked').value;
-        if (value.toLowerCase() === 'language') {
+        if (value.toLowerCase() === 'blue') {
+            setColor("blueBackground");
             showChoice(value);
-        } else {
+        } else if (value.toLowerCase() === 'green') {
+            setColor("greenBackground");
+            showChoice(value);
+        } else if (value.toLowerCase() === 'red') {
+            setColor("redBackground");
+            showChoice(value);
+        } else if (value.toLowerCase() === 'yellow') {
+            setColor("yellowBackground");
+            showChoice(value);
+        } else if (value.toLowerCase() === 'purple') {
+            setColor("purpleBackground");
             showChoice(value);
         }
     });
+
+    let currentClass = null;
+
+    const setColor = (className) => {
+        console.log("SET CLASS TO", className);
+        var element = document.getElementById("form-section");
+        if (currentClass) {
+            element.classList.remove(currentClass);
+        }
+        currentClass = className;
+        element.classList.add(className);
+    }
 }
 
 function showChoice(value) {
